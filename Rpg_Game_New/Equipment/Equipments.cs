c79fba430314f90cfe47a18053526bf5e33cc96d
EelvinafayY <elvinafayzullina2005@icloud.com>
2024-02-19 15:10:00 +0300

    hgn

--- Rpg_Game_New/Equipment/Equipments.cs
+++ Rpg_Game_New/Equipment/Equipments.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Rpg_Game_New.Equipment
 {
-    internal class Equipments
+    public class Equipments 
     {
+        public double Armor_class { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Inteligence { get; set; }
+        public int Vitality { get; set; }
+        public double Health { get; set; }
+        public double Mana { get; set; }
+        public double P_damage { get; set; }
+
+        public Equipments(string type)
+        {
+            Type = type;
+        }
+        public string Name { get; set; }
+        public string Type { get; set; }
     }
 }
